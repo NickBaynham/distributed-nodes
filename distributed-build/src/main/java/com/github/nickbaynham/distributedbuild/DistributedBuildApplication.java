fab96a1ec182883cd7daea3ce77251c402b545a7
Nick Baynham <nick.baynham@oracle.com>
2020-12-31 05:35:58 -0500

    added application code to the repository

---
+++ distributed-build/src/main/java/com/github/nickbaynham/distributedbuild/DistributedBuildApplication.java
@@ -0,0 +1,13 @@
+package com.github.nickbaynham.distributedbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedBuildApplication.class, args);
+	}
+
+}
---
+++ distributed-build/src/test/java/com/github/nickbaynham/distributedbuild/DistributedBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nickbaynham.distributedbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
